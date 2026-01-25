2700e9ef3418d996a538f7c33959dc3299dafc71
Samuel Lúcio <102866875+samucalucio1997@users.noreply.github.com>
2026-01-25 12:42:00 -0300

    Delete exerSB directory

--- exerSB/src/main/java/br/com/Pro/exerSB/repository/ProdutoRepo.java
+++
@@ -1,9 +0,0 @@
-package br.com.Pro.exerSB.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.Pro.exerSB.Models.Produtos;
-
-public interface ProdutoRepo extends JpaRepository<Produtos,Integer>{
-    
-}
