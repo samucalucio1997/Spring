045edacf129807f911c9419c37bac7901cb0dd3b
samucalucio1997 <samucalucio@hotmail.com>
2026-05-13 08:24:10 -0300

    fix: apagando chamadas de libs inutilizadas atualmente

--- src/main/java/com/app/vdc/demo/DemoApplication.java
+++ src/main/java/com/app/vdc/demo/DemoApplication.java
@@ -2,12 +2,10 @@ package com.app.vdc.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
 
 
 @SpringBootApplication
-@EnableKafka
 public class DemoApplication {
  
 	public static void main(String[] args) {
